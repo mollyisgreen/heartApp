64d4e1966bf1e53e07af49b55e87d993877409b7
Molly Yang <mollyisgreen@gmail.com>
2013-10-17 15:28:16 -0400

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HeartApp::Application.config.secret_key_base = '5367b679c878cc4639af918c5f37b9ba99ad4d28ad9dcdcd00a6a99c043b09c623107cafc51113c2f027a7486d3dcd926b077b6e3623be1e0a2af9c289561187'
