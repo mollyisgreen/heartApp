9b0283a37aef088a51d2c9335d6669ed7d86fd5b
Molly Yang <mollyisgreen@gmail.com>
2013-10-18 14:42:59 -0400

    fixing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 HeartApp::Application.routes.draw do
-  get "users/new"
+  resources :users
   root 'static_pages#home'
   match '/signup',   to: 'users#new',   via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-
-  resources :users
 
   get "static_pages/home"
 
